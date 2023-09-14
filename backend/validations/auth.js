f48df23ed94f65d885de3c0e87edc9e275e4e8a7
Zheka <zheseleznev@yandex.ru>
2023-09-14 21:15:34 +0300

    Create base backend functional

---
+++ backend/validations/auth.js
@@ -0,0 +1,10 @@
+import { body } from 'express-validator'
+
+export const registerValidation = [
+    body('email', 'Неверный формат почты').isEmail(),
+    body('password', 'Пароль должен содеражть минимум 5 символов').isLength({
+        min: 5,
+    }),
+    body('fullName', 'Укажите Ваше имя').isLength({ min: 2 }),
+    body('avatarUrl', 'Неверная ссылка на аватарку').optional().isURL(),
+]
