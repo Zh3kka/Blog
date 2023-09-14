ac1b75cc7cb3627db620ef3ed22da6565f429974
Zheka <zheseleznev@yandex.ru>
2023-09-14 21:53:15 +0300

    Created controllers and utils, refactored index.js

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,9 @@
 import express from 'express'
-import jwt from 'jsonwebtoken'
-import bcrypt from 'bcrypt'
-import mongoose from 'mongoose'
+import mongoose, { get } from 'mongoose'
 import { registerValidation } from './validations/auth.js'
-import { validationResult } from 'express-validator'
-import UserModel from './models/User.js'
+import checkAuth from './utils/checkAuth.js'
+import { register, login, getMe } from './controllers/UserController.js'
+
 mongoose
     .connect(
         'mongodb+srv://minus:qqqqqq@cluster0.3zcquqg.mongodb.net/blog?retryWrites=true&w=majority'
@@ -17,45 +16,16 @@ const app = express()
 
 app.use(express.json())
 
-app.post('/auth/register', registerValidation, async (req, res) => {
-    try {
-        const errors = validationResult(req)
-        if (!errors.isEmpty()) {
-            return res.status(400).json(errors.array())
-        }
+app.post('/auth/login', login)
 
-        const password = req.body.password
-        const salt = await bcrypt.genSalt(10)
-        const hashPassword = await bcrypt.hash(password, salt)
+app.post('/auth/register', registerValidation, register)
 
-        const doc = new UserModel({
-            email: req.body.email,
-            fullName: req.body.fullName,
-            avatarUrl: req.body.avatarUrl,
-            passwordHash: hashPassword,
-        })
+app.get('/auth/me', checkAuth, getMe)
 
-        const user = await doc.save()
-
-        const token = jwt.sign(
-            {
-                _id: user._id,
-            },
-            'secret123',
-            {
-                expiresIn: '15d',
-            }
-        )
-
-        const { passwordHash, ...userData } = user._doc
-
-        res.json({ ...userData, token })
-    } catch (error) {
-        console.log(error)
-        return res.status(500).json({
-            message: 'Не удалось зарегистрироваться',
-        })
-    }
+app.get('/', (req, res) => {
+    return res.status(200).json({
+        message: 'Все ок',
+    })
 })
 
 app.listen(4444, (err) => {
